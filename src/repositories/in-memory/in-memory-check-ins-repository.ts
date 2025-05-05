6c9cdc59932632eb6fce5c68193d6aaaf7faeba5
Davi Marques <davimgcb@gmail.com>
2025-05-05 11:40:49 -0300

    feat: tdd and mocking

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -1,9 +1,21 @@
-import { CheckInsRepository } from '@/repositories/check-ins-repository'
+import { CheckInsRepository } from "@/repositories/check-ins-repository";
 import { Prisma, CheckIn } from "generated/prisma";
-import { randomUUID } from 'node:crypto'
+import { randomUUID } from "node:crypto";
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
-  public items: CheckIn[] = []
+  public items: CheckIn[] = [];
+
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const checkInOnSameDate = this.items.find(
+      (checkIn) => checkIn.user_id === userId
+    );
+
+    if (!checkInOnSameDate) {
+      return null;
+    }
+
+    return checkInOnSameDate;
+  }
 
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
@@ -12,10 +24,10 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
       gym_id: data.gym_id,
       validated_at: data.validated_at ? new Date(data.validated_at) : null,
       created_at: new Date(),
-    }
+    };
 
-    this.items.push(checkIn)
+    this.items.push(checkIn);
 
-    return checkIn
+    return checkIn;
   }
 }
