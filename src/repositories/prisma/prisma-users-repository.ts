40bedfa5325202c7723319c39b80810c34ebdf88
Davi Marques <davimgcb@gmail.com>
2025-05-06 20:32:48 -0300

    feat: prisma repository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,6 +3,15 @@ import { Prisma } from "generated/prisma";
 import { UsersRepository } from "../users-repository";
 
 export class PrismaUsersRepository implements UsersRepository {
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return user;
+  }
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
@@ -12,6 +21,7 @@ export class PrismaUsersRepository implements UsersRepository {
 
     return user;
   }
+
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data,
